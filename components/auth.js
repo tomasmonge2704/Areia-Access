28fdabf7c3b819907d70b455f3bb2af32455abed
Tomas Monge <tomasmonge@Tomass-MBP.fibertel.com.ar>
2024-05-18 05:45:19 -0300

    fix ticket route

--- components/auth.js
+++ components/auth.js
@@ -1,47 +1,54 @@
 import { useSession } from "next-auth/react";
 import { createContext, useState, useEffect } from "react";
+import { useRouter } from 'next/router';
 import styles from "../styles/Auth.module.css";
 import Loader from "react-loader-spinner";
 import axios from "axios";
 import { hideEmail, updateEmailUser } from "./ultis";
 import LoginPage from "./login";
+
 export const AuthContext = createContext({
   currentUser: null,
 });
 
 export default function Auth({ children }) {
   const { data: session, status } = useSession();
+  const router = useRouter();
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(undefined);
   const [currentUser, setCurrentUser] = useState(undefined);
   const [dni, setDni] = useState(undefined);
+
   useEffect(async () => {
-      // Si la sesión existe, establece el usuario actual
-      const getDni = localStorage.getItem("dni");
-      setDni(getDni);
-      const { data } = await axios.get(`${process.env.API_URL}/users/${getDni}`);
-      setLoading(false);
-      const { email } = data.mongoUser;
-      if (session?.user?.email && getDni) {
-        if (email === session.user.email) {
-          setCurrentUser(data.mongoUser);
-        }
-        console.log(email);
-        if(!email && session.user) {
-          const newUser = {...data.mongoUser,email:session.user.email}
-          updateEmailUser(newUser);
-          setCurrentUser(newUser);
-        }
-        if(email && email.toLowerCase() !== session.user.email.toLowerCase()){
-          console.log(data?.mongoUser?.email);
-          setError(
-            `El DNI ${dni} está asociado al email ${hideEmail(
-              data?.mongoUser?.email
-            )} y no a ${hideEmail(session.user.email)}`
-          );
-        }        
+    // Si la sesión existe, establece el usuario actual
+    const getDni = localStorage.getItem("dni");
+    setDni(getDni);
+    const { data } = await axios.get(`${process.env.API_URL}/users/${getDni}`);
+    setLoading(false);
+    const { email } = data.mongoUser;
+    if (session?.user?.email && getDni) {
+      if (email === session.user.email) {
+        setCurrentUser(data.mongoUser);
       }
+      console.log(email);
+      if (!email && session.user) {
+        const newUser = { ...data.mongoUser, email: session.user.email }
+        updateEmailUser(newUser);
+        setCurrentUser(newUser);
+      }
+      if (email && email.toLowerCase() !== session.user.email.toLowerCase()) {
+        console.log(data?.mongoUser?.email);
+        setError(
+          `El DNI ${dni} está asociado al email ${hideEmail(
+            data?.mongoUser?.email
+          )} y no a ${hideEmail(session.user.email)}`
+        );
+      }
+    }
   }, [session]);
+
+  // Verifica si la ruta actual contiene 'ticket'
+  const isTicketRoute = router.pathname.includes('ticket');
 
   return (
     <>
@@ -57,17 +64,17 @@ export default function Auth({ children }) {
         </div>
       ) : (
         <>
-          {currentUser ? (
+          {currentUser || isTicketRoute ? (
             <AuthContext.Provider
-            value={{
-              loading,
-              currentUser,
-            }}
-          >
-            {children}
-          </AuthContext.Provider>
+              value={{
+                loading,
+                currentUser,
+              }}
+            >
+              {children}
+            </AuthContext.Provider>
           ) : (
-            <LoginPage err={error}/>
+            <LoginPage err={error} />
           )}
         </>
       )}
